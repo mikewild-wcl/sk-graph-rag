72c91e72a83561a9564e788b1bf02fa1cbf76119
Mike Wild <Michael.Wild@defra.onmicrosoft.com>
2025-09-10 09:15:53 +0100

    Tidy dockerfile and AppHost startup code

--- src/SK.GraphRag.AppHost/AppHost.cs
+++ src/SK.GraphRag.AppHost/AppHost.cs
@@ -1,3 +1,7 @@
+using k8s.KubeConfigModels;
+using MessagePack.Resolvers;
+using SK.GraphRag.AppHost.Extensions;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var neo4jConnection = builder.AddParameter("Neo4jConnection");
@@ -10,29 +14,11 @@ var azureOpenAIDeploymentName = builder.AddParameter("AzureOpenAIDeploymentName"
 var azureOpenAIEmbeddingDeploymentName = builder.AddParameter("AzureOpenAIEmbeddingDeploymentName");
 var azureOpenAITimeout = builder.AddParameter("AzureOpenAITimeout");
 
-var s = neo4jUser.Resource.ToString();
-var userName = await neo4jUser.Resource.GetValueAsync(default).ConfigureAwait(false);
-var password = await neo4jPassword.Resource.GetValueAsync(default).ConfigureAwait(false);
-//var auth = $"NEO4J_AUTH={userName}/{password}";
-var auth = $"{userName}/{password}";
-//var auth = $"NEO4J_AUTH={neo4jUser.Resource.Value}/{neo4jPassword.Resource.Value}";
-//var auth2 = $"NEO4J_AUTH={neo4jUser.GetValueAsync(default).AsTask().GetAwaiter().GetResult()!}/{neo4jPassword}";
-
 builder.AddDockerfile(
     "neo4j", "./", "Dockerfile")
-    .WithEnvironment("NEO4J_AUTH", auth)
-    //.WithEnvironment("DOCKER_BUILDKIT", "1")
     .WithEndpoint(7474, scheme:"http", targetPort:7474)
     .WithEndpoint(7687, scheme:"bolt", targetPort: 7687)
-    //.WithBuildArg("USER", neo4jUser)
-    //.WithBuildSecret("PASSWORD", neo4jPassword)
-    //.WithBuildArg("NEO4J_USER", neo4jUser)
-    //.WithBuildArg("NEO4J_AUTH", auth)
-    //.WithBuildSecret("NEO4J_PASSWORD", neo4jPassword)
-    //.WithBuildSecret("my_secret", neo4jPassword)
-    //.WithBuildArg("NEO4J_PASSWORD", neo4jPassword)
-    //.WithContainerRuntimeArgs("--env", $"NEO4J_AUTH={auth}")
-    ;
+    .WithEnvironment("NEO4J_AUTH", $"{neo4jUser.GetValue()}/{neo4jPassword.GetValue()}");
 
 builder.AddProject<Projects.SK_GraphRag>("sk-graphrag")
     .WithEnvironment("Neo4j:Connection", neo4jConnection)
@@ -42,7 +28,6 @@ builder.AddProject<Projects.SK_GraphRag>("sk-graphrag")
     .WithEnvironment("AzureOpenAI:ApiKey", azureOpenAIApiKey)
     .WithEnvironment("AzureOpenAI:DeploymentName", azureOpenAIDeploymentName)
     .WithEnvironment("AzureOpenAI:EmbeddingDeploymentName", azureOpenAIEmbeddingDeploymentName)
-    .WithEnvironment("AzureOpenAI:Timeout", azureOpenAITimeout)
-    ;
+    .WithEnvironment("AzureOpenAI:Timeout", azureOpenAITimeout);
 
 await builder.Build().RunAsync().ConfigureAwait(false);
---
+++ src/SK.GraphRag.AppHost/Extensions/ResourceBuilderExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace SK.GraphRag.AppHost.Extensions;
+
+internal static class ResourceBuilderExtensions
+{
+    public static string? GetValue(this IResourceBuilder<ParameterResource> parameter) =>
+        parameter.Resource.GetValueAsync(default).AsTask().GetAwaiter().GetResult();
+}
